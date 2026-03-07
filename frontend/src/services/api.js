ca71ca3e5c3b91a246d81cc404f9ebb9171cf29f
mhtraideas <mahin.traideas@gmail.com>
2026-03-07 19:37:55 +0600

    Updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_BASE || 
-  (window.location.hostname === 'localhost' ? 'http://localhost:5000/api' : `${window.location.origin}/api`);
-
+const API_BASE = process.env.REACT_APP_API_BASE || 'https://drop.atysan.xyz/api';
 const api = axios.create({
   baseURL: API_BASE,
 });
